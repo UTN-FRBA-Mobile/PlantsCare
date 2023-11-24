0e8d2ad9314ca0fc0582b434cac37fd4461f0bde
Juan Amarillo <juan.alb.amarillo@gmail.com>
2023-11-24 19:59:02 -0300

    fix/image-id-validation

--- backend/src/images/controllers/Image.controller.ts
+++ backend/src/images/controllers/Image.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Res,
+} from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { Response } from 'express';
+import { isAlphanumeric } from 'class-validator';
+import { rethrow } from '@nestjs/core/helpers/rethrow';
 
 @Controller('images')
 export class ImageController {
   @Get('/plants/:id')
   getPlantImage(@Param('id') id: string, @Res() res: Response) {
+    isAlphanumeric(id) || rethrow(new BadRequestException('invalid image id'));
     const file = createReadStream(`./images/plants/${id}`);
     file.pipe(res);
   }
